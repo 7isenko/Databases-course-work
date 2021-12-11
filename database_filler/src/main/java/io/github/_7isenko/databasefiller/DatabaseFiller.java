41099c9db5b377b1a0874609f66d69e249c09867
7isenko <7isenko.max@gmail.com>
2021-12-11 16:58:47 +0300

    checked a possibility to get data

--- database_filler/src/main/java/io/github/_7isenko/databasefiller/DatabaseFiller.java
+++ database_filler/src/main/java/io/github/_7isenko/databasefiller/DatabaseFiller.java
@@ -1,11 +1,53 @@
 package io.github._7isenko.databasefiller;
 
+import com.sun.istack.internal.Nullable;
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * @author 7isenko
  */
 public class DatabaseFiller {
+
+    private static final int MAX_SCP_ID = 4000;
+
     public static void main(String[] args) {
 
+        /*
+        make args for file
+         */
+
+
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        for (int i = 0; i < 2; i++) {
+            int scpId = random.nextInt(1, MAX_SCP_ID);
+            Document doc = receiveDocFromId(scpId);
+            if (doc == null) continue;
+
+            // TODO: есть страницы в необычном формате. Их игнорировать.
+            Element name = doc.getElementById("page-title");
+            System.out.println(name != null ? name.text().split(" ", 3)[2] : null);
+        }
+    }
+
+    @Nullable
+    private static Document receiveDocFromId(int scpId) {
+        try {
+            String link = String.format("http://scp-ru.wikidot.com/scp-%03d", scpId);
+            return Jsoup.connect(link).get();
+        } catch (HttpStatusException e) {
+            if (e.getStatusCode() == 404)
+                System.out.println(String.format("Объект SCP-%03d не существует", scpId));
+            return null;
+        } catch (IOException e) {
+            System.out.println(String.format("Проблемы с интернет-соединением: %s", e.getMessage()));
+            return null;
+        }
     }
 
 }
