5e2d86d0ca6a321fcfa1317c736b396e76d2535a
7isenko <7isenko.max@gmail.com>
2021-12-11 17:49:17 +0300

    data receiving enhancing

--- database_filler/src/main/java/io/github/_7isenko/databasefiller/DatabaseFiller.java
+++ database_filler/src/main/java/io/github/_7isenko/databasefiller/DatabaseFiller.java
@@ -5,8 +5,12 @@ import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.ThreadLocalRandom;
 
 /**
@@ -15,38 +19,112 @@ import java.util.concurrent.ThreadLocalRandom;
 public class DatabaseFiller {
 
     private static final int MAX_SCP_ID = 4000;
+    private static final Set<Integer> SCP_IDS = new HashSet<>();
 
     public static void main(String[] args) {
 
         /*
-        make args for file
+         make args for file
          */
+        ArrayList<SCPDocument> scpDocuments = null;
+        try {
+            scpDocuments = receiveScpDocuments(10);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
 
+        for (int i = 1; i <= scpDocuments.size(); i++) {
+            SCPDocument scpDocument = scpDocuments.get(i - 1);
 
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        for (int i = 0; i < 2; i++) {
-            int scpId = random.nextInt(1, MAX_SCP_ID);
-            Document doc = receiveDocFromId(scpId);
-            if (doc == null) continue;
+            Document document = scpDocument.getDocument();
+            int scpId = scpDocument.getId();
 
-            // TODO: есть страницы в необычном формате. Их игнорировать.
-            Element name = doc.getElementById("page-title");
-            System.out.println(name != null ? name.text().split(" ", 3)[2] : null);
+            String name = getNameFromDocument(document);
+            String description = getDescriptionFromDocument(document);
+
+            if (name == null || description == null) {
+                System.out.printf("Страница объекта №%d - SCP-%03d имеет нестандартную структуру. Она будет пропущена.%n%n", i, scpId);
+                continue;
+            }
+
+            System.out.printf("Объект %d:%n", i);
+            System.out.printf("SCP-%03d - %s%n", scpId, name);
+            System.out.printf("%s%n%n", description);
         }
     }
 
+    private static int getRandomSCPNumber() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        int randomId;
+        do {
+            randomId = random.nextInt(1, MAX_SCP_ID);
+        } while (SCP_IDS.contains(randomId));
+        SCP_IDS.add(randomId);
+        return randomId;
+    }
+
     @Nullable
-    private static Document receiveDocFromId(int scpId) {
+    private static String getNameFromDocument(Document document) {
+        Element nameElement = document.getElementById("page-title");
+        if (nameElement == null) return null;
+        return nameElement.text().split(" ", 3)[2];
+    }
+
+    @Nullable
+    private static String getDescriptionFromDocument(Document document) {
+        for (Element element : document.select("#page-content p")) {
+            Elements strong = element.getElementsByTag("strong");
+            if (strong.size() == 0) continue;
+            if (strong.get(0).text().startsWith("Описание")) {
+                return element.text().substring(10);
+            }
+        }
+        return null;
+    }
+
+    private static ArrayList<SCPDocument> receiveScpDocuments(int amount) throws IOException {
+        ArrayList<SCPDocument> documents = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+            int scpId = getRandomSCPNumber();
+            Document doc = receiveDocumentByScpId(scpId);
+            if (doc == null) {
+                i--;
+            } else {
+                documents.add(new SCPDocument(scpId, doc));
+            }
+        }
+        return documents;
+    }
+
+
+    @Nullable
+    private static Document receiveDocumentByScpId(int scpId) throws IOException {
         try {
             String link = String.format("http://scp-ru.wikidot.com/scp-%03d", scpId);
             return Jsoup.connect(link).get();
         } catch (HttpStatusException e) {
-            if (e.getStatusCode() == 404)
-                System.out.println(String.format("Объект SCP-%03d не существует", scpId));
             return null;
-        } catch (IOException e) {
-            System.out.println(String.format("Проблемы с интернет-соединением: %s", e.getMessage()));
-            return null;
+        }
+    }
+
+
+    private static class SCPDocument {
+        private final int id;
+        private final Document document;
+
+        private SCPDocument(int id, Document document) {
+            this.id = id;
+            this.document = document;
+        }
+
+
+        public int getId() {
+            return id;
+        }
+
+        public Document getDocument() {
+            return document;
         }
     }
 
