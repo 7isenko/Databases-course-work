0fd19c0cb62df65668c6719059de901bf68ba07e
7isenko <7isenko.max@gmail.com>
2022-01-22 22:19:15 +0300

    большой базированный template for index and controller

---
+++ SCP1985/src/main/java/io/github/_7isenko/SCP1985/controllers/IndexController.java
@@ -0,0 +1,32 @@
+package io.github._7isenko.SCP1985.controllers;
+
+import io.github._7isenko.SCP1985.model.entities.ScpObjectEntity;
+import io.github._7isenko.SCP1985.model.repositories.ScpObjectEntityRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * @author 7isenko
+ */
+@Controller
+public class IndexController {
+
+    // Must be static to be visible
+    private static List<ScpObjectEntity> scpEntities;
+
+
+    public IndexController(ScpObjectEntityRepository scpObjectEntityRepository) {
+        scpEntities = scpObjectEntityRepository.findAll();
+    }
+
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+    public String index(Model model) {
+        model.addAttribute("scpEntities", scpEntities);
+        return "index";
+    }
+
+}
