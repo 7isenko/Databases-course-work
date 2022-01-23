84ad76bcba006fd2c555757368f47a5c1fb1149b
PiMat-maker <54264954+PiMat-maker@users.noreply.github.com>
2022-01-23 22:20:31 +0300

    Added extends to ExcursionLogTypeRepository

--- SCP1985/src/main/java/io/github/_7isenko/SCP1985/model/repositories/ExcursionLogTypeRepository.java
+++ SCP1985/src/main/java/io/github/_7isenko/SCP1985/model/repositories/ExcursionLogTypeRepository.java
@@ -1,13 +1,14 @@
 package io.github._7isenko.SCP1985.model.repositories;
 
 import io.github._7isenko.SCP1985.model.object_types.ExcursionLogType;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ExcursionLogTypeRepository {
+public interface ExcursionLogTypeRepository extends JpaRepository<ExcursionLogType, Integer> {
 
     @Procedure(value = "make_reports")
     List<ExcursionLogType> makeReports();
