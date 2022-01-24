db5b2aa71c29df97baa3540e2e3d3bcd01b67c90
7isenko <7isenko.max@gmail.com>
2022-01-25 01:55:03 +0300

    added error and logout message with GET-request for login page

--- SCP1985/src/main/java/io/github/_7isenko/SCP1985/controllers/LoginController.java
+++ SCP1985/src/main/java/io/github/_7isenko/SCP1985/controllers/LoginController.java
@@ -2,9 +2,6 @@ package io.github._7isenko.SCP1985.controllers;
 
 import io.github._7isenko.SCP1985.controllers.forms.LoginForm;
 import io.github._7isenko.SCP1985.model.repositories.PersonnelEntityRepository;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author 7isenko
@@ -33,19 +31,34 @@ public class LoginController {
 
     @RequestMapping(value = {"/login"}, method = RequestMethod.GET)
     public String login(Model model) {
-        LoginForm loginForm = new LoginForm();
-        model.addAttribute("loginForm", loginForm);
+        addLoginForm(model);
 
         return "login";
     }
 
-    @RequestMapping(value = {"/", "/login"}, method = RequestMethod.POST)
-    public ResponseEntity<String> login(@ModelAttribute("loginForm") LoginForm loginForm) {
+    @RequestMapping(value = {"/login"}, method = RequestMethod.GET, params = "error")
+    public String loginWithError(@RequestParam("error") String err, Model model) {
+        addLoginForm(model);
+
+        model.addAttribute("error", "Неправильное имя или пароль.");
+        return "login";
+    }
+
+    @RequestMapping(value = {"/login"}, method = RequestMethod.GET, params = "logout")
+    public String loginWithLogout(@RequestParam("logout") String logout, Model model) {
+        addLoginForm(model);
+
+        model.addAttribute("logout", "Вы успешно вышли из профиля.");
+        return "login";
+    }
+
+    @RequestMapping(value = {"/login"}, method = RequestMethod.POST)
+    public String login(@ModelAttribute("loginForm") LoginForm loginForm) {
         String name = loginForm.getName();
         String key = loginForm.getKey();
 
         if (personnelEntityRepository.findPersonnelEntityByFullName(name) == null) {
-            return new ResponseEntity<>(("Invalid."), HttpStatus.UNAUTHORIZED);
+            return "redirect:login?error";
         }
 
         UsernamePasswordAuthenticationToken authReq = new UsernamePasswordAuthenticationToken(name, key);
@@ -53,9 +66,12 @@ public class LoginController {
         SecurityContext sc = SecurityContextHolder.getContext();
         sc.setAuthentication(auth);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Location", "/main"); // TODO: сделать страничку с навигацией?
-        return new ResponseEntity<>(headers, HttpStatus.FOUND);
+        return "navigation";
+    }
+
+    private void addLoginForm(Model model) {
+        LoginForm loginForm = new LoginForm();
+        model.addAttribute("loginForm", loginForm);
     }
 
 }
