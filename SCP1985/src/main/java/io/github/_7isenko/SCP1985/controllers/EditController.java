bc00ed25e88e772f44c8e701eec7dd6e5b967cb3
PiMat-maker <54264954+PiMat-maker@users.noreply.github.com>
2022-01-24 15:55:46 +0300

    Create update in editController

--- SCP1985/src/main/java/io/github/_7isenko/SCP1985/controllers/EditController.java
+++ SCP1985/src/main/java/io/github/_7isenko/SCP1985/controllers/EditController.java
@@ -1,6 +1,8 @@
 package io.github._7isenko.SCP1985.controllers;
 
+import io.github._7isenko.SCP1985.model.entities.ExcursionLogEntity;
 import io.github._7isenko.SCP1985.model.object_types.ExcursionLogType;
+import io.github._7isenko.SCP1985.model.repositories.ExcursionLogEntityRepository;
 import io.github._7isenko.SCP1985.model.repositories.ExcursionLogTypeRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,17 +13,36 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
-/**
+/**..
  * @author 7isenko
  */
 @Controller
 public class EditController {
 
     private final ExcursionLogTypeRepository reportsRepository;
+    private List<ExcursionLogEntity> excursionLogEntities;
+    private final ExcursionLogEntityRepository excursionLogEntityRepository;
 
 
-    public EditController(ExcursionLogTypeRepository reportsRepository) {
+    public EditController(ExcursionLogTypeRepository reportsRepository,
+                          ExcursionLogEntityRepository excursionLogEntityRepository) {
         this.reportsRepository = reportsRepository;
+        this.excursionLogEntityRepository = excursionLogEntityRepository;
+    }
+
+    @RequestMapping(value = {"/edit"}, method = RequestMethod.POST)
+    public String update(@ModelAttribute("report") ExcursionLogType report){
+        reportsRepository.updateExcursionLog(report.getId(), report.getReality_description(),
+                                                report.getLog_status().toString(), report.getNote());
+        excursionLogEntities = excursionLogEntityRepository.findAll();
+        int retrieval_id = 0;
+        for (ExcursionLogEntity excursionLog : excursionLogEntities){
+            if (excursionLog.getId() == report.getId()){
+                retrieval_id = excursionLog.getRetrievalId();
+            }
+        }
+        reportsRepository.updateTimeBackToFoundation(report.getReturn_to_foundation(), retrieval_id);
+        return "redirect:report";
     }
 
     @Transactional
--- SCP1985/src/main/java/io/github/_7isenko/SCP1985/model/repositories/ExcursionLogTypeRepository.java
+++ SCP1985/src/main/java/io/github/_7isenko/SCP1985/model/repositories/ExcursionLogTypeRepository.java
@@ -1,10 +1,16 @@
 package io.github._7isenko.SCP1985.model.repositories;
 
 import io.github._7isenko.SCP1985.model.object_types.ExcursionLogType;
+import io.github._7isenko.SCP1985.model.object_types.LogStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.jpa.repository.query.Procedure;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+import java.sql.Timestamp;
 import java.util.List;
 
 @Repository
@@ -12,4 +18,16 @@ public interface ExcursionLogTypeRepository extends JpaRepository<ExcursionLogTy
 
     @Procedure(value = "make_reports")
     List<ExcursionLogType> makeReports();
+
+    @Transactional
+    @Modifying
+    @Query(value = "update excursion_log SET reality_description = :descr, log_status = :status, " +
+            "note = :note WHERE id = :id", nativeQuery = true)
+    void updateExcursionLog(@Param("id") int excursion_log_id, @Param("descr") String reality_description,
+                            @Param("status") String log_status, @Param("note") String note);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update retrieval SET return_to_foundation = :time WHERE id = :id", nativeQuery = true)
+    void updateTimeBackToFoundation(@Param("time") Timestamp return_to_foundation, @Param("id") int retrieval_id);
 }
