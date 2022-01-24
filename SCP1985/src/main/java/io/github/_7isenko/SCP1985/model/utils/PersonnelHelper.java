a83917d04e6c95142e32393777bf5aab28c692dd
7isenko <7isenko.max@gmail.com>
2022-01-24 03:15:54 +0300

    fixed integrity constraint

--- SCP1985/src/main/java/io/github/_7isenko/SCP1985/model/utils/PersonnelHelper.java
+++ SCP1985/src/main/java/io/github/_7isenko/SCP1985/model/utils/PersonnelHelper.java
@@ -38,9 +38,9 @@ public class PersonnelHelper {
             }
             ClassificationWithWeight cl = items[idx];
             int level = 0;
-            if (cl.name.equals("A")) level = ThreadLocalRandom.current().nextInt(5, 6);
+            if (cl.name.equals("A")) level = ThreadLocalRandom.current().nextInt(5, 7);
             if (cl.name.equals("B")) level = ThreadLocalRandom.current().nextInt(4, 6);
-            if (cl.name.equals("C")) level = ThreadLocalRandom.current().nextInt(3, 5);
+            if (cl.name.equals("C")) level = ThreadLocalRandom.current().nextInt(3, 6);
             if (cl.name.equals("D")) level = 1;
 
             personnels.add(new PersonnelEntity(faker.name().firstName(), faker.name().lastName(), ClearanceLevel.values()[level], Classification.valueOf(cl.name)));
